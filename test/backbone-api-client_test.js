03c0c8d153cb7ba0a209da66cde81efb72ee0d4b
Todd Wolfson <todd@twolfson.com>
2014-04-26 21:37:19 -0700

    Starting to prove out repo via test against GitHub

--- test/backbone-api-client_test.js
+++ test/backbone-api-client_test.js
@@ -1,13 +1,40 @@
+var GitHubApi = require('github');
+var Backbone = require('backbone');
 var BackboneApiClient = require('../');
 
-describe('A BackboneApiClient-mixed model using GitHub\'s API client', function () {
-  before(function () {
+// TODO: Use eightTrack/express/http in front of GitHub
 
+describe('A BackboneApiClient-mixed model using GitHub\'s API client', function () {
+  before(function createGitHub () {
+    var UserModel = BackboneApiClient.mixinModel(Backbone).extend({
+      resourceName: 'user',
+      callApiClient: function (method, options, cb) {
+        if (method === 'read') {
+          return this.apiClient[this.resourceName].get(options, cb);
+        } else {
+          throw new Error('We have not yet implemented "' + method + '" for `UserModel`');
+        }
+      }
+    });
   });
 
-  it('', function () {
+  describe('fetching data', function () {
+    before(function () {
 
+    });
+
+    it('retrieves data from the API', function () {
+
+    });
+  });
+
+  describe('failing to retrieve data', function () {
+    it('calls back with an error', function () {
+
+    });
   });
 });
 
 // TODO: Test the entirety of methods (e.g. create, read, update, patch, delete)
+
+// TODO: Test collections
